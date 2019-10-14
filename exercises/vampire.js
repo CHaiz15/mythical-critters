727365c2188da6f442a80ffe8adb49de9ef64491
caleb.haizlett@gmail.com <alanturing@example.c>
2019-10-14 10:15:43 -0600

    Complete vampire

---
+++ exercises/vampire.js
@@ -0,0 +1,20 @@
+class Vampire {
+  constructor(name, pet){
+    this.name = name;
+    if (pet === undefined){
+    this.pet = 'bat';
+  }
+  else {
+    this.pet = pet;
+  }
+  this.thirsty = true
+}
+  drink(){
+    if (this.thirsty = false){
+      return true;
+    }
+  }
+}
+
+
+module.exports = Vampire;
